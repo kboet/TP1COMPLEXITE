ccf1824390afbe941a5fb844fc3bfec635e3868c
e16006212 <mohammed.eneim@etu.univ-amu.fr>
2022-09-14 17:07:22 +0200

    fibonacci version iterative

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,5 +1,22 @@
-public class Fibonacci {
-    public public static void main(String[] args) {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
+public class Fibonacci {
+    public static void main(String[] args) {
+        maSuite(10);
+    }
+
+    public static ArrayList maSuite(int n){
+        ArrayList suite= new ArrayList();
+        int temp1=0, temp2=1;
+        int sum;
+        for (int i= 0; i <= n; i++){
+            sum = temp1 + temp2;
+            temp1 = temp2;
+            temp2 = sum;
+            suite.add(sum);
+            System.out.println(suite.get(i));
+        }
+        return suite;
     }
 }

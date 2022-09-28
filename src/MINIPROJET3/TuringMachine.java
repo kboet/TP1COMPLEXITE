c75fe5be39b922f9880d2911498e0a3e1b8ec21e
kboet <teo.blaisekaplanski@icloud.com>
2022-09-28 16:17:43 +0200

    MAJ

--- src/MINIPROJET3/TuringMachine.java
+++ src/MINIPROJET3/TuringMachine.java
@@ -1,5 +1,22 @@
 package MINIPROJET3;
 
 public class TuringMachine {
+
+    String mot;
+    int state, i;
     
+
+
+    public TuringMachine(String mot){
+        this.mot = mot;
+    }
+
+    public void process(){
+        for (i = 0; i < mot.length(); i++){
+            switch (mot.charAt(i)){
+
+            }
+        }
+    }
+
 }
